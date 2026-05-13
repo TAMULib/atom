a95d0cc6bac2a4140ae1ee407e94a2366021e173
Douglas Hahn <dhahn@tamu.edu>
2026-05-13 15:18:12 -0500

    Update bootstrap.php

--- docker/bootstrap.php
+++ docker/bootstrap.php
@@ -320,10 +320,11 @@ user = root
 group = root
 listen = [::]:9000
 pm = dynamic
-pm.max_children = 5
-pm.start_servers = 2
-pm.min_spare_servers = 1
-pm.max_spare_servers = 3
+pm.max_children = 100
+pm.start_servers = 20
+pm.min_spare_servers = 20
+pm.max_spare_servers = 40
+pm.max_requests = 500
 
 EOT;
 
